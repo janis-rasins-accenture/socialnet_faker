a52f64ddb65eb21e55c3d4e214226e8a6084eef9
Janis Rasins <janis@developer.lv>
2022-10-06 09:29:08 +0300

    Adding Posts v.1.2.0

--- generate.js
+++ generate.js
@@ -4,12 +4,17 @@ import fs from "node:fs/promises"
 
 let users = []
 let messages = []
+let posts = []
 const usersCount = 20
 const minMessagesCount = 2
 const maxMessagesCount = 10
 const maxMessageSentences = 3
 const minMessageDate = 1577836800000
 const maxMessageDate = Date.now()
+const maxPostsCount = 5
+const maxTitleLength = 4
+const postImageWidth = 480
+const postImageHeight = 270
 
 const createRandomUser = (userId) => {
 	const gender = faker.datatype.boolean() ? "male" : "female"
@@ -66,17 +71,38 @@ const generateUniqueNumbersArray = (arrayLength, maxNumber) => {
 	return uniqueArray
 }
 
+const generateMessage = () => {
+	const date = faker.datatype.datetime({ min: minMessageDate, max: maxMessageDate })
+	return {
+		text: faker.lorem.sentences(faker.datatype.number({ max: maxMessageSentences, min: 1 })),
+		timestamp: date.getTime(),
+		isOwn: faker.datatype.boolean(),
+	}
+}
+
 const generateUserMessages = (messagesCount) => {
-	let messages = Array.from({ length: messagesCount }).map(() => {
-		const date = faker.datatype.datetime({ min: minMessageDate, max: maxMessageDate })
-		return {
-			text: faker.lorem.sentences(faker.datatype.number({ max: maxMessageSentences, min: 1 })),
-			timestamp: date.getTime(),
-			isOwn: faker.datatype.boolean(),
-		}
-	})
+	let messages = Array.from({ length: messagesCount }).map(() => generateMessage())
 	messages.sort((a, b) => sortByNumber(a.timestamp, b.timestamp))
 	return messages
+}
+
+const generatePost = (id, userId) => ({
+	id: id,
+	userId: userId,
+	title: faker.lorem.sentence(faker.datatype.number({ max: maxTitleLength, min: 2 })),
+	text: faker.lorem.text(),
+	imageUrl: faker.image.abstract(postImageWidth, postImageHeight, true),
+})
+
+let lastPostId = 1
+
+const createPosts = (userId) => {
+	const postCount = faker.datatype.number({ max: maxPostsCount, min: 1 })
+	let newPost = Array.from({ length: postCount }).map((el, index) =>
+		generatePost(index + lastPostId, userId)
+	)
+	posts = [...posts, ...newPost]
+	lastPostId = lastPostId + postCount
 }
 
 const sortByNumber = (a, b) => {
@@ -89,8 +115,10 @@ const sortByNumber = (a, b) => {
 }
 
 Array.from({ length: usersCount }).forEach((el, index) => {
-	createRandomUser(index + 1)
-	createMessages(index + 1)
+	let userId = index + 1
+	createRandomUser(userId)
+	createMessages(userId)
+	createPosts(userId)
 })
 
 const saveData = async (content, filename) => {
@@ -102,6 +130,7 @@ const saveData = async (content, filename) => {
 }
 
 saveData(JSON.stringify(users, null, "\t"), "users").then(() => console.log("Finished users!"))
+saveData(JSON.stringify(posts, null, "\t"), "posts").then(() => console.log("Finished posts!"))
 saveData(JSON.stringify(messages, null, "\t"), "messages").then(() =>
 	console.log("Finished messages!")
 )
